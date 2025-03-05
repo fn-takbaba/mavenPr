b7e0c5c01e895662cf2fc135a14cf646f50d7811
mtlgkbb20 <musmeh10@gmail.com>
2025-03-05 08:08:25 +0300

    last

--- src/main/java/Anotherfile.java
+++ src/main/java/Anotherfile.java
@@ -3,5 +3,7 @@ public class Anotherfile {
     public static void main(String[] args) {
         	  System.err.println("");
               String a = "";
+              System.out.print("hey");
     }
 }
+
